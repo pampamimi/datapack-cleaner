c16f9b2149cc51c950c8b71819f9cce77ddaa299
Varnilla <reidev241@gmail.com>
2022-04-10 22:19:02 +0700

    Completed index.js and readme file.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# How to use it
+# What It Does
+It basically just looping through every files in the specifed Minecraft datapack folder and then modify the content of its file by removing space and comments!
+
+# How To Use It
+1. Download [NodeJS](https://nodejs.org/en/download/) if you haven't already
+2. Download this repository
+3. Move the folder of this repository inside the datapack folder (alongside `pack.mcmeta`)
+4. Run `start.bat` if you're on Windows, Otherwise run `node .` inside the `datapack-cleaner` folder
+5. Every comments and spaces are gone from the files :)
---
+++ index.js
@@ -0,0 +1,56 @@
+const { readdir, stat, readFile, writeFile } = require("fs");
+
+function sendErr(err) {
+
+    console.clear()
+
+    console.log("Error has occured, please contact Varnilla#2065 on Discord. Send this error message below!\n\n" + err.message)
+
+    process.exit(0)
+
+}
+
+function loop(directoryPath) {
+
+    readdir(directoryPath, (err, files) => {
+
+        if (err) sendErr(err)
+
+        files.forEach(fileOrFolder => {
+
+            stat(directoryPath + fileOrFolder, (err, stat) => {
+                
+                if (err) sendErr(err)
+
+                if (stat.isDirectory()) loop(directoryPath + fileOrFolder + "/")
+
+                else if (fileOrFolder.endsWith(".mcfunction")) {
+
+                    readFile(directoryPath + fileOrFolder, { encoding: 'utf-8' }, (err, content) => {
+
+                        if (content.length == 0) return console.log(`Removed 0 lines from ${fileOrFolder} because its empty`)
+
+                        if (err) sendErr(err)
+
+                        const qualified = []
+                        content.split(/\r?\n/).forEach(line => {
+                            if(line.startsWith("#") || !line) return
+                            qualified.push(line)
+                        })
+
+                        writeFile(directoryPath + fileOrFolder, qualified.join("\n"), err => {
+
+                            if (err) sendErr(err)
+
+                        })
+
+                        console.log(`Removed ${content.split(/\r?\n/).length - qualified.length} lines from ${fileOrFolder}`)
+
+                    })
+                }
+            })
+        })
+    })
+}
+
+loop("../")
